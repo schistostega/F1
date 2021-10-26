974748ebe4a68667b92e0a2eb6da0016460c1745
schistos <schistos@example.com>
2021-10-26 23:01:55 +0300

    amend driver rating component
    - add error handling in aura component for driver rating
    - add links to record pages in Drivers Name column

--- force-app/main/default/aura/DriverRating/DriverRatingHelper.js
+++ force-app/main/default/aura/DriverRating/DriverRatingHelper.js
@@ -1,8 +1,28 @@
 ({
 	getDrivers : function(component, event, helper) {
 		const action = component.get("c.getTop5Drivers");
+        // error handling
         action.setCallback(this, function(response) {
-            component.set("v.drivers", response.getReturnValue());
+            const state = response.getState();
+            
+            if (state === 'SUCCESS') {
+                component.set("v.isError", false);
+                const drivers = response.getReturnValue();
+                for (let driver of drivers) {
+                    driver.url = `/lightning/r/Driver__c/${driver.Id}/view`;
+                }
+                component.set("v.drivers", drivers);
+            } else if (state === 'ERROR') {
+                component.set("v.isError", true);
+
+                const errors = response.getError();
+                if (errors) {
+                    if (errors[0]) {
+                        console.error(errors[0]);
+                    }
+                }
+                
+            }
         });
         
         $A.enqueueAction(action);
