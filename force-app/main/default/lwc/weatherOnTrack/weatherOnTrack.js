02af6f99643dc888bcc182b3f540e86cbe31011c
schistos <schistos@example.com>
2021-10-31 22:25:39 +0200

    add error handling to weatherOnTrack component

--- force-app/main/default/lwc/weatherOnTrack/weatherOnTrack.js
+++ force-app/main/default/lwc/weatherOnTrack/weatherOnTrack.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import getWeatherOnTrack from '@salesforce/apex/FormulaOneServices.getWeatherOnTrack';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class TrackWeather extends LightningElement {
     @api recordId;
@@ -13,8 +14,36 @@ export default class TrackWeather extends LightningElement {
         })
         .then(resp => {
             const data = JSON.parse(resp);
+
+            if (data.statusCode !== 200) {
+                this.temp = 'x(';
+
+                const message = data.message ?? 'Try to reload page. If error still remain, contact your administrator.';
+
+                const evt = new ShowToastEvent({
+                    title: 'Error Occured',
+                    message: 'Sorry, error has been occured. ' + message,
+                    variant: 'error',
+                });
+                this.dispatchEvent(evt);
+
+                return;
+            }
+
             const celsius = Math.round((data.temp - 32) / 1.8);
             this.temp = 'Temp: ' + celsius + '°C';       
         })
+        .catch(err => {
+            this.temp = 'x(';
+
+            const evt = new ShowToastEvent({
+                title: 'Error Occured',
+                message: 'Sorry, error has been occured. Try to reload page. If error still remain, contact your administrator.',
+                variant: 'error',
+            });
+            this.dispatchEvent(evt);
+
+            console.error(err);
+        });
     }
 }
