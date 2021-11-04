da7d7a74f40de10eb33ca49619110212082e0d54
schistos <schistos@example.com>
2021-11-04 15:26:02 +0200

    remove hardcoding links in lwc component relatedResultsList

--- force-app/main/default/lwc/relatedResultsList/relatedResultsList.js
+++ force-app/main/default/lwc/relatedResultsList/relatedResultsList.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import getWinsOfDriver from '@salesforce/apex/FormulaOneServices.getWinsOfDriver';
+import { NavigationMixin } from 'lightning/navigation';
 
 const columns = [
     { label: 'Race Result Name', fieldName: 'Name', },
@@ -7,7 +8,7 @@ const columns = [
     { label: 'Is Fastest Lap', fieldName: 'Is_Fastest_Lap__c', type: 'boolean' },
 ];
 
-export default class RelatedResultsList extends LightningElement {
+export default class RelatedResultsList extends NavigationMixin(LightningElement) {
     @api recordId;
     data = [];
     columns = columns;
@@ -16,25 +17,30 @@ export default class RelatedResultsList extends LightningElement {
     isEmpty = false;
 
     async connectedCallback() {
-        getWinsOfDriver({
-            driverId: this.recordId
-        })
-        .then(resp => {
+        try {
+            const resp = await getWinsOfDriver({driverId: this.recordId});
+
             if (resp.length === 0) {
                 this.isEmpty = true;
                 return;
             }
-
-            for (let item of resp) {
+    
+            for (let item of resp) {      
+                let url = await this[NavigationMixin.GenerateUrl]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: item.Grand_Prix__r.Id,
+                        actionName: 'view',
+                    },
+                });
+                item.Grand_Prix_Link = url;
                 item.Grand_Prix_Name = item.Grand_Prix__r.Name;
-                item.Grand_Prix_Link = `/lightning/r/Race__c/${item.Grand_Prix__r.Id}/view`;
             }
             
             this.data = resp;
-        })
-        .catch(err => {
+        } catch (err) {
             this.isError = true;
             console.error(err);
-        });
+        }
     }    
 }
